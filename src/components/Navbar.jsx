b2214e44cac7faf7f275e613e92daf5305c60308
Samin Amanat <amanat361@gmail.com>
2023-06-24 12:16:53 -0700

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className="flex flex-row justify-between items-center w-full h-16 bg-Primary border-b-4 border-Outline">
+            Sup
+        </div>
+    );
+}
