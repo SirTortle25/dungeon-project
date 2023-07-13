793ed7dcd8971ba6179870f9d41f2c51470b1cc1
SirTortle25 <juliangheinen@gmail.com>
2023-07-12 17:31:22 -0700

    Co-authored-by: Samin Amanat <amanat361@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './App.css';
 import Navbar from './components/Navbar'
@@ -8,21 +8,37 @@ import CardForm from './components/CardForm';
 
 function App() {
 
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const savedCards = localStorage.getItem('cards');
+    setCards(savedCards ? JSON.parse(savedCards) : []);
+  }, []); // load cards
+
+  useEffect(() => {
+    if (cards.length === 0) return;
+    localStorage.setItem('cards',JSON.stringify(cards));
+  }, [cards]); // save cards
 
   function addCard(new_card) {
-    setCards([...cards, new_card])
+    setCards([...cards, new_card]);
   }
 
   function clearCards(e) {
-    e.preventDefault()
-    setCards([])
+    e.preventDefault();
+    setCards([]);
+    localStorage.removeItem('cards');
+  }
+
+  function removeCard(index) {
+    console.log(index);
+    setCards(cards.filter(card => cards.indexOf(card) )
   }
     
   return (
     <div className="flex flex-col h-screen relative">
         <Navbar />
-        <CardContainer cards={cards}/>
+        <CardContainer cards={cards} removeCard={removeCard}/>
         <CardForm addFunction={addCard} clearCards={clearCards}/>
     </div>
   );
